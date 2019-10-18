c587880ff89c62fe7eaa37b7ae2dd3e1ccd53574
rajesh747 <56711874+rajesh747@users.noreply.github.com>
2019-10-18 13:08:24 +0530

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -23,7 +23,40 @@ oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\
       USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-# TBD
+echo "
+apiVersion: v1
+items:
+- kind: "BuildConfig"
+  apiVersion: "v1"
+  metadata:
+    name: "pipeline-demo"
+  spec:
+    triggers:
+          - github:
+              secret: 5Mlic4Le
+            type: GitHub
+          - generic:
+              secret: FiArdDBH
+            type: Generic
+    strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+        jenkinsfile: |
+                          node {
+                              stage ("Build")
+                                    echo '*** Build Starting ***'
+                                    openshiftBuild bldCfg: 'cotd2', buildName: '', checkForTriggeredDeployments: 'false', commitID: '', namespace: '', showBuildLogs: 'true', verbose: 'true'
+                                    openshiftVerifyBuild bldCfg: 'cotd2', checkForTriggeredDeployments: 'false', namespace: '', verbose: 'false'
+                                    echo '*** Build Complete ***'
+                              stage ("Deploy and Verify in Development Env")
+                                    echo '*** Deployment Starting ***'
+                                    openshiftDeploy depCfg: 'cotd2', namespace: '', verbose: 'false', waitTime: ''
+                                    openshiftVerifyDeployment authToken: '', depCfg: 'cotd2', namespace: '', replicaCount: '1', verbose: 'false', verifyReplicaCount: 'false', waitTime: ''
+                                    echo '*** Deployment Complete ***'
+                               }
+
+kind: List
+metadata: {}" | oc create -f - -n ${GUID}-jenkins
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
